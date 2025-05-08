f286659a8a2456cb1a00dc82ed9cd1da3bdbdb21
Victoria Malyants <1132246740@pfur.ru>
2025-05-08 14:22:38 +0300

    feat(main): add files lab13

---
+++ labs/lab13/lab13-3.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+for ((i=1; i<=$*; i++))
+do
+if test -f "$i".tmp
+then rm "$i".tmp
+else touch "$i.tmp"
+fi 
+done
---
+++ labs/lab13/lab13-4.sh
@@ -0,0 +1,2 @@
+find $* -mtime -7 -mtime +0 -type f > FILES.txt
+tar -cf archive.tar -T FILES.txt
